40da66845ff05fa21025564ad97864c9052fea40
Sahib <sahib.singh@zabbed.com>
2017-07-07 16:00:18 +0530

    Minor updates with imports

--- main.py
+++ main.py
@@ -1,11 +1,9 @@
-import numpy as np
 import pandas as pd
-from sklearn.cross_validation import train_test_split
+from sklearn.model_selection import train_test_split
 import keras
 from keras.models import Sequential
 from keras.layers import Dense, Dropout, Flatten
 from keras.layers import Conv2D, MaxPooling2D
-import keras.utils.np_utils as kutils
 
 batch_size = 5000
 num_classes = 10
