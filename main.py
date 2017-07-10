bc02d33995302b36a4eb9f6ef8440ad1a01334a8
Sahib <sahib.singh@zabbed.com>
2017-07-11 01:08:40 +0530

    98.38% Accuracy after 100 epoch..wohooo!

--- main.py
+++ main.py
@@ -2,11 +2,9 @@ import pandas as pd
 from sklearn.model_selection import train_test_split
 import keras
 from keras.models import Sequential
-from keras.layers import Dense, Dropout, Flatten
-from keras.layers import Conv2D, MaxPooling2D
-import numpy as np
+from keras.layers import Dense, Dropout, Flatten, Conv2D, MaxPooling2D
 
-batch_size = 50
+batch_size = 64
 num_classes = 10
 epochs = 100
 
@@ -19,7 +17,7 @@ input_shape = (img_rows, img_cols, 1)
 
 # Reshaping feature data for 2d Conv. layer
 x_train = file[:, 1:].reshape(file.shape[0], img_rows, img_cols, 1)
-x_train = x_train.astype(float)
+x_train = x_train.astype('float32')
 x_train /= 255.0
 
 # Converting output data
@@ -30,16 +28,16 @@ x_train, x_val, y_train, y_val = train_test_split(x_train, y_train, test_size=0.
 
 # Building Model
 model = Sequential()
-model.add(Conv2D(128, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(128, kernel_size=(6, 6), activation='relu', input_shape=input_shape))
+model.add(Conv2D(64, kernel_size=(6, 6), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.25))
-model.add(Conv2D(64, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
-model.add(MaxPooling2D(pool_size=(2, 2)))
-model.add(Dropout(0.25))
-model.add(Conv2D(32, kernel_size=(3, 3), activation='relu', input_shape=input_shape))
+model.add(Conv2D(32, kernel_size=(3, 3), activation='relu'))
 model.add(MaxPooling2D(pool_size=(2, 2)))
 model.add(Dropout(0.2))
 model.add(Flatten())
+model.add(Dense(128, activation='relu'))
+model.add(Dropout(0.4))
 model.add(Dense(num_classes, activation='softmax'))
 
 model.compile(loss="categorical_crossentropy",
@@ -52,8 +50,10 @@ model.fit(x_train, y_train,
           verbose=1,
           validation_data=(x_val, y_val))
 
-score = model.evaluate(x_val, y_val, verbose=0)
+model.save('model.h5')
+print('Model Saved!')
 
+score = model.evaluate(x_val, y_val, verbose=0)
 print('Validation loss:', score[0])
 
 x_test = test.reshape(test.shape[0], 28, 28, 1)
@@ -62,7 +62,7 @@ x_test /= 255.0
 
 predictions = model.predict_classes(x_test)
 
-submissions = pd.DataFrame({"ImageId": list(range(1, len(predictions)+1)), "Label": predictions})
+submissions = pd.DataFrame({"ImageId": list(range(1, len(predictions) + 1)), "Label": predictions})
 
 submissions.to_csv("Prediction.csv", index=False, header=True)
 
